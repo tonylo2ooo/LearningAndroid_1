895c7c07cd2d64b57f50275868bac30f69685416
Tony LO <tonylo2000@hotmail.com>
2011-07-08 14:58:11 +0800

    v2

--- gen/com/Tony/HelloWorld/R.java
+++ gen/com/Tony/HelloWorld/R.java
@@ -18,10 +18,9 @@ public final class R {
         public static final int test1=0x7f020004;
     }
     public static final class id {
-        public static final int clicktimes=0x7f050001;
-        public static final int imageView1=0x7f050002;
-        public static final int tableLayout1=0x7f050000;
-        public static final int timer=0x7f050003;
+        public static final int Click=0x7f050001;
+        public static final int clicktimes=0x7f050000;
+        public static final int timer=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/Tony/HelloWorld/HelloWorld.java
+++ src/com/Tony/HelloWorld/HelloWorld.java
@@ -3,41 +3,39 @@ package com.Tony.HelloWorld;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.*;
 import android.os.CountDownTimer;
 
-public class HelloWorld extends Activity {
+public class HelloWorld extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
-	private static int[] bgs = { R.drawable.graph1, R.drawable.graph2 };
     public int times=0;
     public boolean clickable=true;
     public boolean startable=true;
     public TextView textShown;
     public TextView timerShown;
     public CountDownTimer count;
+    public Button click;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);        
-    	ImageView image1 = (ImageView)findViewById(R.id.imageView1);
-    	image1.setImageDrawable(getResources().getDrawable(bgs[0]));
     	textShown = (TextView)findViewById(R.id.clicktimes);
-        timerShown = (TextView)findViewById(R.id.timer);    	
+        timerShown = (TextView)findViewById(R.id.timer); 
+        click = (Button)findViewById(R.id.Click);
+        click.setOnClickListener(this);
         
         count = new CountDownTimer(5000, 1000) {
-
             public void onTick(long millisUntilFinished) {
-            	timerShown.setText("seconds remaining: " + millisUntilFinished / 1000);
+            	timerShown.setText(millisUntilFinished / 1000 + "\nseconds\nremained: ");
             }
-
             public void onFinish() {
             	timerShown.setText("done!");
             	clickable=false;
-            	
             }
          };
-        
+          
     }
-    public void imbt1(View imbt1View){
+    public void onClick(View v){
     	if(startable==true){
     		count.start();
     	}
@@ -47,12 +45,8 @@ public class HelloWorld extends Activity {
     	}
     	String tmp = Integer.toString(times);
     	textShown.setText(tmp);
-    	
     }
 }
-
-
-
 
 
 
